961fd169a31396b3cf3976d86747a737b263086e
Ian Johnson <itj3@nau.edu>
2014-01-23 12:30:39 -0700

    Added GUI in python.
    Note GUI_old is what we used to C++ and GUI.glade is what we will use
    for python.

---
+++ GUI.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import gtk
+from matplotlib.figure import Figure
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+
+
+
+class GUI:
+	wTree = gtk.Builder()
+
+	def __init__( self ):
+		#import glade file
+		self.builder = gtk.Builder()
+		self.builder.add_from_file("GUI.glade")
+		
+		#define widgets from glade file
+		self.windowMain = self.builder.get_object("windowMain")
+		self.hbox1 = self.builder.get_object("hbox1")
+		
+		#a simple matplotlib graph
+		f = Figure(figsize=(5,4), dpi=100)
+		a = f.add_subplot(111)
+		t = arange(0.0,3.0,0.01)
+		s = sin(2*pi*t)
+		a.plot(t,s)
+		
+		#convert graph to drawingArea which GTK can use
+		graph = FigureCanvas(f)  # a gtk.DrawingArea
+
+		#pack graph into GUI
+		self.hbox1.pack_start(graph, True, True, 0)
+		
+		#connect events and show all widgets in GUI
+		self.windowMain.connect("destroy", gtk.main_quit)
+		self.windowMain.show_all()
+
+
+
+#Main function
+if __name__ == "__main__":
+	GUI()
+	gtk.main()
+
+
